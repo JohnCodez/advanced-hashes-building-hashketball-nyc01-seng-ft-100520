d12ffe1dfdd11be894fd2e37d3d7e15d135ac864
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-09-30 20:16:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,7 +66,7 @@ def game_hash
       team_name: "",
       colors: [],
       players: [{
-        player_name: "",
+        player_name: "Jeff Adrien",
         number: "",
         shoe: "",
         points: "", 
